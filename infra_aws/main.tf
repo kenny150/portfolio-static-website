f48462b01d79ee332b920d51f97319a2c0bc00b8
Kennedy Alves <alvkennedy@gmail.com>
2026-04-21 19:28:55 -0300

    Add public IP

--- infra_aws/main.tf
+++ infra_aws/main.tf
@@ -19,10 +19,11 @@ resource "aws_instance" "ec2_nginx" {
   ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
   instance_type = "t3.micro"
   key_name      = "ec2-instance"
+  associate_public_ip_address = true
 
-  primary_network_interface {
-    network_interface_id = aws_network_interface.ec2_nginx_interface.id
-  }
+  # primary_network_interface {
+  #   network_interface_id = aws_network_interface.ec2_nginx_interface.id
+  # }
 
   tags = {
     Name = "ec2-instance"
